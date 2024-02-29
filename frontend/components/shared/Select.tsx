dc4271e08a0057e78199dfaf4cf3038267d8bf82
shadinmhd <shadinmhd97@gmail.com>
2024-02-28 18:01:27 -0800

    update: create select component from shadcn select

---
+++ frontend/components/shared/Select.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState } from "react"
+import { ShadSelect, SelectTrigger, SelectContent, SelectGroup, SelectItem } from "../shadcn/ShadSelect"
+
+interface Props {
+	items: string[],
+	onSelect: (item: string) => void
+}
+
+const Select = ({ items, onSelect }: Props) => {
+	const [selected, setSelected] = useState("Sort by")
+
+	return (
+		<ShadSelect onValueChange={(e) => onSelect(e)}>
+			<SelectTrigger className="outline-none">
+				<p>
+					{selected}
+				</p>
+			</SelectTrigger>
+			<SelectContent className="outline-none">
+				<SelectGroup className="outline-none">
+					{
+						items.map((e, i) => (
+							<SelectItem value={e} key={i}>
+								{e}
+							</SelectItem>
+						))
+					}
+				</SelectGroup>
+			</SelectContent>
+		</ShadSelect>
+	)
+}
+
+export default Select
