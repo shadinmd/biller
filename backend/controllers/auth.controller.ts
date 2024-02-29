61de8c5bda2bb5d5a1c1d58d75ff21fae4da1a02
shadinmhd <shadinmhd97@gmail.com>
2024-02-28 17:56:51 -0800

    update: added all auth routes and admin login

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,8 +1,50 @@
 import { Request, Response } from "express"
 import { handle500ServerError } from "../libs/error.handlers"
+import AdminModel from "../models/admin.model"
+import jwt from "jsonwebtoken"
 
-export const adminLogin = (req: Request, res: Response) => {
+export const adminLogin = async (req: Request, res: Response) => {
 	try {
+		const { username, password } = req.body
+
+		if (!username || !password) {
+			res.status(400).send({
+				success: false,
+				message: "please fill all fields"
+			})
+			return
+		}
+
+		const adminSearch = await AdminModel.findOne({ username })
+
+		if (!adminSearch) {
+			res.status(400).send({
+				success: false,
+				message: `admin ${username} not found`
+			})
+			return
+		}
+
+		if (password != adminSearch.password) {
+			res.status(400).send({
+				success: false,
+				message: `incorrect username or password`
+			})
+		}
+
+		const payload = {
+			id: adminSearch._id,
+			admin: true
+		}
+
+		const token = jwt.sign(payload, process.env.JWT_SECRET as string)
+
+		res.status(200).send({
+			success: true,
+			message: "logged in successfully",
+			token
+		})
+
 
 	} catch (error) {
 		console.log(error)
@@ -12,6 +54,7 @@ export const adminLogin = (req: Request, res: Response) => {
 
 export const vendorLogin = (req: Request, res: Response) => {
 	try {
+		const { email, password } = req.body
 
 	} catch (error) {
 		console.log(error)
@@ -21,6 +64,7 @@ export const vendorLogin = (req: Request, res: Response) => {
 
 export const vendorRegister = (req: Request, res: Response) => {
 	try {
+		const { } = req.body
 
 	} catch (error) {
 		console.log(error)
@@ -30,6 +74,7 @@ export const vendorRegister = (req: Request, res: Response) => {
 
 export const staffLogin = (req: Request, res: Response) => {
 	try {
+		const { username, password, shopId } = req.body
 
 	} catch (error) {
 		console.log(error)
--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,4 +1,10 @@
 import express from "express"
+import { adminLogin, staffLogin, vendorLogin, vendorRegister } from "../controllers/auth.controller"
 const authRouter = express.Router()
+
+authRouter.post("/vendor/login", vendorLogin)
+authRouter.post("/vendor/register", vendorRegister)
+authRouter.post("/staff/login", staffLogin)
+authRouter.post("/admin/login", adminLogin)
 
 export default authRouter
