afb24c0555989b7db7ebc2575cc6a290deed8d96
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-03-09 19:01:35 +0530

    update: added security and erro handling for axios instance
    added code to populate Authorization header field depending on the route
    
    created a new interceptor for response to check for auth related errors
    before reaching the page

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -3,12 +3,56 @@ import { toast } from "sonner";
 
 const api = axios.create({ baseURL: "http://localhost:8000/api" })
 
-api.interceptors.request.use((request) => {
-	if (localStorage.getItem("vendor-token")) {
-		request.headers.Authorization = localStorage.getItem("vendor-token")
+if (location.href.split("/")[3] == "vendor")
+	api.interceptors.request.use((request) => {
+		if (localStorage.getItem("vendor-token")) {
+			request.headers.Authorization = localStorage.getItem("vendor-token")
+		}
+		return request
+	})
+
+if (location.href.split("/")[3] == "staff")
+	api.interceptors.request.use((request) => {
+		if (localStorage.getItem("staff-token")) {
+			request.headers.Authorization = localStorage.getItem("staff-token")
+		}
+		return request
+	})
+
+if (location.href.split("/")[3] == "admin")
+	api.interceptors.request.use((request) => {
+		if (localStorage.getItem("admin-token")) {
+			request.headers.Authorization = localStorage.getItem("admin-token")
+		}
+		return request
+	})
+
+api.interceptors.response.use(
+	(res) => res,
+	(error) => {
+		if (isAxiosError(error)) {
+			if (error?.response?.data?.error == "blocked") {
+				localStorage.removeItem("token")
+				toast.error("your account is blocked")
+				window.location.assign("/login")
+
+			} else if (error.response?.data.error == "invalidtoken") {
+				localStorage.removeItem("token")
+				window.location.assign("/login")
+
+			} else if (error.response?.data.error == "unauthorized") {
+				error.response.data.types.forEach((e: string) => {
+					localStorage.removeItem(`${e}-token`)
+				})
+				location.href = "/"
+			}
+			return Promise.reject(error)
+		} else {
+			console.log(error)
+			return Promise.reject(error)
+		}
 	}
-	return request
-})
+)
 
 export const handleAxiosError = (error: any) => {
 	if (isAxiosError(error)) {
