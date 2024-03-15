a796b8c090a777de86523ebee42ec55c555120e1
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-03-15 19:39:24 +0530

    update: forgoted commit for staff detail fetching

--- backend/routes/staff.route.ts
+++ backend/routes/staff.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createStaff, getAllStaffsByshop } from "../controllers/staff.controller";
+import { createStaff, getAllStaffsByshop, getStaffDetails } from "../controllers/staff.controller";
 import authorizationMiddleware from "../middlewares/authorization.middleware";
 
 const staffRouter = Router()
@@ -7,7 +7,10 @@ const staffRouter = Router()
 staffRouter.route("/")
 	.post(authorizationMiddleware("vendor", "staff"), createStaff)
 
-staffRouter.route("/:id")
+staffRouter.route("/shop/:id")
 	.get(authorizationMiddleware("vendor", "staff"), getAllStaffsByshop)
+
+staffRouter.route("/:id")
+	.get(authorizationMiddleware("vendor", "staff"), getStaffDetails)
 
 export default staffRouter
