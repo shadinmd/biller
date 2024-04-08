9ab8787e0f3ee6886f81c0212592674bdfd56b2e
shadinmhd <shadinmhd97@gmail.com>
2024-04-08 07:29:32 +0530

    update: added className to edit trigger for new product

--- frontend/components/shared/NewProduct.tsx
+++ frontend/components/shared/NewProduct.tsx
@@ -7,12 +7,14 @@ import { z } from "zod"
 import { toast } from "sonner"
 import ProductInterface from "types/product.interface"
 import { AxiosInstance } from "axios"
+import cn from "@/lib/cn"
 
 interface Props {
 	children: ReactNode,
 	shopId: string,
 	newProduct: (product: ProductInterface) => void,
-	api: AxiosInstance
+	api: AxiosInstance,
+	className?: string
 }
 
 const formSchema = z.object({
@@ -36,7 +38,7 @@ type className = string
 
 const inputStyle: className = "border-2 border-primary px-3 py-1 rounded-lg outline-none"
 
-const NewProduct = ({ children, shopId, newProduct, api }: Props) => {
+const NewProduct = ({ children, shopId, newProduct, api, className }: Props) => {
 
 	const { register, handleSubmit, formState: { errors } } = useForm<formType>({
 		resolver: zodResolver(formSchema),
@@ -48,11 +50,23 @@ const NewProduct = ({ children, shopId, newProduct, api }: Props) => {
 			point: 0
 		}
 	})
+	const [image, setImage] = useState<File>()
 	const [open, setOpen] = useState(false)
 
 	const onSubmit = async (data: formType) => {
+
+		const form = new FormData()
+		if (image) {
+			form.append("file", image || "no file")
+		}
+
+		for (const [key, value] of Object.entries(data)) {
+			form.append(key, value.toString());
+		}
+		form.append("shopId", shopId)
+
 		try {
-			const response = await api.post("/product", { shopId, ...data })
+			const response = await api.post("/product", form)
 			if (response.data.success) {
 				toast.success(response.data.message)
 				newProduct(response.data.product)
@@ -67,10 +81,10 @@ const NewProduct = ({ children, shopId, newProduct, api }: Props) => {
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger className="outline-none">
+			<DialogTrigger className={cn("outline-none", className)}>
 				{children}
 			</DialogTrigger>
-			<DialogContent className="bg-white">
+			<DialogContent className="bg-white outline-none">
 				<DialogHeader>
 					<DialogTitle>
 						New Product
@@ -79,70 +93,88 @@ const NewProduct = ({ children, shopId, newProduct, api }: Props) => {
 						create a new product in this shop
 					</DialogDescription>
 				</DialogHeader>
-				<div>
-					<form
-						onSubmit={handleSubmit(onSubmit)}
-						className="flex flex-col items-center gap-2"
-					>
-						<div className="flex gap-3 items-center">
-							<p>Name: </p>
-							<input
-								{...register("name")}
-								placeholder="Name"
-								type="text"
-								className={inputStyle}
-							/>
-						</div>
-						{errors.name && <p className="text-red-500">{errors.name.message}</p>}
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className="flex flex-col items-center gap-2"
+				>
+					<div className="flex items-center justify-center w-full h-full">
+						<label htmlFor="image" className="flex items-center justify-center scale-56">
+							{
+								image ?
+									<img className="bg-contain" height={300} width={300} src={URL.createObjectURL(image)} alt="" /> :
+									<p className="text-white font-bold bg-primary px-6 py-2 rounded-full">
+										pick a photo
+									</p>
+							}
+						</label>
+						<input
+							id="image"
+							onChange={e => { e.target.files && setImage(e.target.files[0]) }}
+							type="file"
+							multiple={false}
+							className=""
+							hidden
+						/>
 
-						<div className="flex gap-3 items-center">
-							<p>Barcode: </p>
-							<input
-								{...register("barcode")}
-								placeholder="Barcode"
-								type="text"
-								className={inputStyle}
-							/>
-						</div>
-						{errors.barcode && <p className="text-red-500">{errors.barcode.message}</p>}
+					</div>
+					<div className="flex gap-3 items-center">
+						<p>Name: </p>
+						<input
+							{...register("name")}
+							placeholder="Name"
+							type="text"
+							className={inputStyle}
+						/>
+					</div>
+					{errors.name && <p className="text-red-500">{errors.name.message}</p>}
 
-						<div className="flex gap-3 items-center">
-							<p>Price: </p>
-							<input
-								{...register("price", { valueAsNumber: true })}
-								placeholder="Price"
-								type="text"
-								className={inputStyle}
-							/>
-						</div>
-						{errors.price && <p className="text-red-500">{errors.price.message}</p>}
-						<div className="flex gap-3 items-center">
-							<p>Profit: </p>
-							<input
-								{...register("profit", { valueAsNumber: true })}
-								placeholder="Profit"
-								type="text"
-								className={inputStyle}
-							/>
-						</div>
-						{errors.profit && <p className="text-red-500">{errors.profit.message}</p>}
-						<div className="flex gap-3 items-center">
-							<p>Point: </p>
-							<input
-								{...register("point", { valueAsNumber: true })}
-								placeholder="Point"
-								type="text"
-								className={inputStyle}
-							/>
-						</div>
-						{errors.point && <p className="text-red-500">{errors.point.message}</p>}
-						<button className="font-bold text-white bg-primary px-6 py-2 rounded-full" type="submit">
-							Create
-						</button>
-					</form>
-				</div>
+					<div className="flex gap-3 items-center">
+						<p>Barcode: </p>
+						<input
+							{...register("barcode")}
+							placeholder="Barcode"
+							type="text"
+							className={inputStyle}
+						/>
+					</div>
+					{errors.barcode && <p className="text-red-500">{errors.barcode.message}</p>}
+
+					<div className="flex gap-3 items-center">
+						<p>Price: </p>
+						<input
+							{...register("price", { valueAsNumber: true })}
+							placeholder="Price"
+							type="text"
+							className={inputStyle}
+						/>
+					</div>
+					{errors.price && <p className="text-red-500">{errors.price.message}</p>}
+					<div className="flex gap-3 items-center">
+						<p>Profit: </p>
+						<input
+							{...register("profit", { valueAsNumber: true })}
+							placeholder="Profit"
+							type="text"
+							className={inputStyle}
+						/>
+					</div>
+					{errors.profit && <p className="text-red-500">{errors.profit.message}</p>}
+					<div className="flex gap-3 items-center">
+						<p>Point: </p>
+						<input
+							{...register("point", { valueAsNumber: true })}
+							placeholder="Point"
+							type="text"
+							className={inputStyle}
+						/>
+					</div>
+					{errors.point && <p className="text-red-500">{errors.point.message}</p>}
+					<button className="font-bold text-white bg-primary px-6 py-2 rounded-full" type="submit">
+						Create
+					</button>
+				</form>
 			</DialogContent>
-		</Dialog>
+		</Dialog >
 	)
 }
 
