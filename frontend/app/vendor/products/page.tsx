81e9083281cdb48ea30ca975033e2d6b9c3c6efb
shadinmhd <shadinmhd97@gmail.com>
2024-04-08 07:30:54 +0530

    update: changed product list theme

--- frontend/app/vendor/products/page.tsx
+++ frontend/app/vendor/products/page.tsx
@@ -6,7 +6,6 @@ import { handleAxiosError } from "@/lib/api"
 import { vendorApi } from "@/lib/vendorApi"
 import { useEffect, useState } from "react"
 import ProductInterface from "types/product.interface"
-import { Separator } from "@/components/shadcn/Seperator"
 import Link from "next/link"
 import { ScaleLoader } from "react-spinners"
 import { useVendor } from "@/context/vendorContext"
@@ -15,24 +14,31 @@ const Products = () => {
 
 	const [products, setProducts] = useState<ProductInterface[]>([])
 	const [loading, setLoading] = useState(true)
+	const [search, setSearch] = useState("")
 	const { vendor } = useVendor()
 
 	useEffect(() => {
-		if (vendor.shop)
-			vendorApi.get(`/product/shop/${vendor.shop}`)
-				.then(({ data }) => {
-					if (data.success) {
-						setProducts(data.products)
-					} else {
-						toast.error(data.message)
-					}
-				})
-				.catch(error => {
-					handleAxiosError(error)
-				}).finally(() => {
-					setLoading(false)
-				})
-	}, [vendor.shop])
+		const timeout = setTimeout(() => {
+			if (vendor.shop)
+				vendorApi.get(`/product/shop/${vendor.shop}?name=${search}`)
+					.then(({ data }) => {
+						if (data.success) {
+							setProducts(data.products)
+						} else {
+							toast.error(data.message)
+						}
+					})
+					.catch(error => {
+						handleAxiosError(error)
+					}).finally(() => {
+						setLoading(false)
+					})
+		}, 500)
+
+		return () => {
+			clearTimeout(timeout)
+		}
+	}, [vendor.shop, search])
 
 	const newProduct = (product: ProductInterface) => {
 		setProducts(products => [...products, product])
@@ -47,36 +53,45 @@ const Products = () => {
 	}
 
 	return (
-		<div className='flex flex-col py-3 px-5 w-full h-full bg-white rounded-lg drop-shadow-lg'>
+		<div className='flex flex-col gap-1 py-3 px-5 w-full h-full'>
 			<div className='flex justify-between'>
-				<p className='text-xl font-bold'>Products</p>
-				<NewProduct shopId={vendor.shop || ""} newProduct={newProduct} api={vendorApi}>
+				<div className="flex items-center gap-1">
+					<p className='flex items-center justify-center text-xl font-bold bg-white drop-shadow-lg rounded-lg p-2'>Products</p>
+					<input
+						value={search}
+						onChange={e => setSearch(e.target.value)}
+						placeholder="Search.."
+						type="text"
+						className="rounded-lg py-2 px-3 drop-shadow-lg outline-none font-semibold"
+					/>
+				</div>
+				<NewProduct className="bg-white rounded-lg drop-shadow-lg" shopId={vendor.shop || ""} newProduct={newProduct} api={vendorApi}>
 					<Icon icon={"mdi:plus"} className='text-4xl text-green-500' />
 				</NewProduct>
 			</div>
-			<div className='flex text-custom-light-gray justify-between w-full'>
-				<p className="w-full">Name</p>
-				<p className="w-full">price</p>
-				<p className="w-full">stock</p>
-			</div>
-			<Separator orientation="horizontal" className="w-full bg-custom-light-gray opacity-60" />
-			{products.map((e, i) => (
-				<div
-					key={`staff-${i}`}
-					className="flex flex-col items-center w-full"
-				>
+			<div className="flex flex-col gap-1">
+				{products.map((e, i) => (
 					<Link
 						href={`/vendor/products/${e?._id}`}
-						className='flex items-center justify-between w-full h-10'
+						key={i}
+						className="flex items-center w-full rounded-lg bg-white drop-shadow-lg p-2 font-semibold"
 					>
+						<div className="pr-2">
+							<div className={`flex items-center justify-center size-10 rounded-lg`}>
+								{
+									e.image ?
+										<img src={e.image} className="h-full w-full" alt="" /> :
+										<Icon icon={"mdi:image"} className="text-gray-500 text-4xl" />
+								}
+							</div>
+						</div>
 						<p className="w-full">{e.name}</p>
 						<p className="w-full">{e.price}</p>
 						<p className="w-full">{e.stock}</p>
 					</Link>
-					<Separator orientation="horizontal" className="w-full bg-custom-light-gray opacity-60" />
-				</div>
-			))}
-		</div>
+				))}
+			</div>
+		</div >
 
 	)
 }
