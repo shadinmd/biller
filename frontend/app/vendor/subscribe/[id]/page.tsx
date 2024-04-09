b90d60757ab4cf6313390faf09128352028fa0cb
shadinmhd <shadinmhd97@gmail.com>
2024-04-09 07:58:30 +0530

    update: changed theme for subscribe page

--- frontend/app/vendor/subscribe/[id]/page.tsx
+++ frontend/app/vendor/subscribe/[id]/page.tsx
@@ -15,7 +15,6 @@ interface Props {
 }
 
 const Subscribe = ({ params }: Props) => {
-
 	const router = useRouter()
 	const today = new Date(Date.now())
 	const expiryDate = new Date().setMonth(today.getMonth() + 1)
@@ -30,7 +29,6 @@ const Subscribe = ({ params }: Props) => {
 		active: false,
 		features: [],
 		productLimit: 0,
-		shopLimit: 0,
 		billLimit: 0,
 	})
 
@@ -80,49 +78,57 @@ const Subscribe = ({ params }: Props) => {
 	}
 
 	return (
-		<div className='flex flex-col items-center gap-5 h-full w-full p-5 rounded-lg bg-white drop-shadow-lg'>
-			<div className='flex items-center'>
-				<p className='text-3xl font-bold'>Subscribe</p>
-			</div>
-			<div className='flex gap-5 font-semibold items-start text-xl flex-col h-full'>
-				<div className="flex flex-col gap-2 self-center items-start bg-white rounded-lg drop-shadow-lg p-5">
-					<div className="flex items-center justify-between gap-5 w-full">
-						<div className="flex flex-col">
-							<p className="text-2xl font-bold">{plan.name}</p>
-							<span className="w-full h-1 bg-primary rounded-full"></span>
-						</div>
-						<div className="flex flex-col items-end">
-							<p className="text-3xl font-bold">₹{plan.price - plan.discount}/M</p>
-							<p className="font-bold line-through decoration-red-500">₹{plan.price}/M</p>
-						</div>
-					</div>
-					<p className="text-lg break-words font-bold">
-						{plan.description}
-					</p>
-					<ul className="flex list-disc pl-5 font-bold flex-col h-full">
-						<li>Max shops: {plan.shopLimit}</li>
-						<li>Max bills: {plan.billLimit}</li>
-						<li>Max products: {plan.productLimit}</li>
-						<li>24/7 customer support</li>
-						<li>online data backup</li>
-					</ul>
-				</div>
-				<div className='flex items-center gap-2'>
+		<div className='flex items-center gap-5 h-full w-full p-5'>
+			<div className='flex flex-col gap-2 items-center h-full w-full font-semibold'>
+				<p className='text-3xl font-bold bg-white rounded-lg w-full drop-shadow-lg p-2'>Subscribe</p>
+				<div className='flex items-center gap-2 bg-white rounded-lg drop-shadow-lg p-2 w-full'>
 					<p>Purchase date: </p>
 					<p>{moment(today).local().format("hh:mm A on DD/MM/YYYY")}</p>
 				</div>
-				<div className='flex items-center gap-2'>
+				<div className='flex items-center gap-2 bg-white rounded-lg drop-shadow-lg p-2 w-full'>
 					<p>Expiration date: </p>
 					<p>{moment(expiryDate).local().format("hh:mm A on DD/MM/YYYY")}</p>
 				</div>
 			</div>
-			<div className='flex justify-center w-full'>
+			<div className='flex flex-col gap-2 w-full h-full'>
+				<PlanCard plan={plan} />
 				<button onClick={e => { e.preventDefault(); subscribe() }} className='bg-primary rounded-lg px-6 py-3 font-bold text-white'>
 					Subscribe
 				</button>
 			</div>
 		</div>
 	)
+}
+
+interface CardProps {
+	plan: PlanInterface
+}
+
+const PlanCard = ({ plan }: CardProps) => {
+	return (
+		<div className="flex flex-col gap-2 font-semibold bg-white rounded-lg drop-shadow-lg p-5">
+			<div className="flex items-center justify-between gap-5 w-full">
+				<div className="flex flex-col">
+					<p className="text-2xl font-bold">{plan.name}</p>
+					<span className="w-full h-1 bg-primary rounded-full"></span>
+				</div>
+				<div className="flex flex-col items-end">
+					<p className="text-3xl font-bold">₹{plan.price - plan.discount}/M</p>
+					<p className="font-bold line-through decoration-red-500">₹{plan.price}/M</p>
+				</div>
+			</div>
+			<p className="text-lg break-words font-bold">
+				{plan.description}
+			</p>
+			<ul className="flex list-disc pl-5 font-bold flex-col h-full">
+				<li>Max bills: {plan.billLimit}</li>
+				<li>Max products: {plan.productLimit}</li>
+				<li>24/7 customer support</li>
+				<li>online data backup</li>
+			</ul>
+		</div>
+	)
+
 }
 
 export default Subscribe
