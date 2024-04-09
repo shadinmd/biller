8332a744a369af83b738c528dd634a1ca13d9c8c
shadinmhd <shadinmhd97@gmail.com>
2024-04-09 08:03:48 +0530

    update: added data for admin dashboard

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -14,7 +14,6 @@ import { useEffect, useState } from 'react';
 import { handleAxiosError } from '@/lib/api';
 import { toast } from 'sonner';
 import { adminApi } from '@/lib/adminApi';
-import moment from 'moment';
 
 ChartJS.register(
 	CategoryScale,
@@ -28,8 +27,77 @@ ChartJS.register(
 const Vendor = () => {
 
 	const [data, setData] = useState<{ _id: string, count: number }[]>([])
+	const [billData, setBillData] = useState<{ _id: string, count: number }[]>([])
+	const [subCount, setSubCount] = useState(0)
+	const [labels, setLables] = useState<string[]>([])
+	const [vendorCount, setVendorCount] = useState(0)
 
 	useEffect(() => {
+		adminApi.get("/subscribe/analytics")
+			.then(({ data }) => {
+				if (data.success) {
+					if (data.data.length > labels.length) {
+						let temp = data.data.map((e: { _id: string }) => e._id)
+						while (temp.length < 5) {
+							temp.unshift("no_data")
+						}
+						setLables(temp)
+						setLables(data.data.map((e: { _id: string }) => e._id))
+					}
+					while (data.data.length < 5) {
+						data.data.unshift({ _id: 'no_data', count: 0 })
+					}
+					setData(data.data)
+				} else {
+					toast.error(data.message)
+				}
+			}).catch(error => {
+				handleAxiosError(error)
+			})
+
+		adminApi.get("/bill/analytics")
+			.then(({ data }) => {
+				if (data.success) {
+					if (data.bills.length > labels.length) {
+						let temp = data.bills.map((e: { _id: string }) => e._id)
+						while (temp.length < 5) {
+							temp.unshift("no_data")
+						}
+						setLables(temp)
+					}
+					while (data.bills.length < 5) {
+						data.bills.unshift({ _id: 'no_data', count: 0 })
+					}
+					setBillData(data.bills)
+					console.log(data.bills)
+				} else {
+					toast.error(data.message)
+				}
+			}).catch(error => {
+				handleAxiosError(error)
+			})
+
+		adminApi.get("/subscribe/count")
+			.then(({ data }) => {
+				if (data.success) {
+					setSubCount(data.subs)
+				} else {
+					toast.error(data.message)
+				}
+			}).catch(error => {
+				handleAxiosError(error)
+			})
+
+		adminApi.get("/vendor/count")
+			.then(({ data }) => {
+				if (data.success) {
+					setVendorCount(data.vendors)
+				} else {
+					toast.error(data.message)
+				}
+			}).catch(error => {
+				handleAxiosError(error)
+			})
 	}, [])
 
 	return (
@@ -39,7 +107,7 @@ const Vendor = () => {
 				<div className="flex items-center justify-between p-4 bg-white rounded-lg drop-shadow-lg w-full">
 					<div>
 						<p className="text-custom-light-gray">vendors</p>
-						<p className="font-bold">0</p>
+						<p className="font-bold">{vendorCount}</p>
 					</div>
 					<div className="flex items-center justify-center bg-primary rounded-xl w-[40px] h-[40px]">
 						<Icon icon={"material-symbols:contract"} className="text-white text-2xl" />
@@ -49,7 +117,7 @@ const Vendor = () => {
 				<div className="flex items-center justify-between p-4 bg-white rounded-lg drop-shadow-lg w-full">
 					<div>
 						<p className="text-custom-light-gray">subscriptions</p>
-						<p className="font-bold">0</p>
+						<p className="font-bold">{subCount}</p>
 					</div>
 					<div className="flex items-center justify-center bg-primary rounded-xl w-[40px] h-[40px]">
 						<Icon icon={"material-symbols:contract"} className="text-white text-2xl" />
@@ -58,8 +126,8 @@ const Vendor = () => {
 
 				<div className="flex items-center justify-between p-4 bg-white rounded-lg drop-shadow-lg w-full">
 					<div>
-						<p className="text-custom-light-gray">bills</p>
-						<p className="font-bold">9</p>
+						<p className="text-custom-light-gray">bills today</p>
+						<p className="font-bold">{billData[billData.length - 1]?.count || 0}</p>
 					</div>
 					<div className="flex items-center justify-center bg-primary rounded-xl w-[40px] h-[40px]">
 						<Icon icon={"material-symbols:contract"} className="text-white text-2xl" />
@@ -68,8 +136,8 @@ const Vendor = () => {
 
 				<div className="flex items-center justify-between p-4 bg-white rounded-lg drop-shadow-lg w-full">
 					<div>
-						<p className="text-custom-light-gray">new subs</p>
-						<p className="font-bold">0</p>
+						<p className="text-custom-light-gray">subs today</p>
+						<p className="font-bold">{data[data.length - 1]?.count || 0}</p>
 					</div>
 					<div className="flex items-center justify-center bg-primary rounded-xl w-[40px] h-[40px]">
 						<Icon icon={"material-symbols:contract"} className="text-white text-2xl" />
@@ -80,14 +148,23 @@ const Vendor = () => {
 			<div className='w-full h-full bg-white drop-shadow-lg rounded-lg p-5'>
 				<Bar
 					datasetIdKey='id'
+					options={{
+						maintainAspectRatio: false
+					}}
 					data={{
-						labels: data.map(e => e._id),
+						labels: labels,
 						datasets: [
 							{
 								label: "subs",
 								data: data.map(e => e.count),
 								backgroundColor: "#3F488D",
 								borderColor: "#3F488D"
+							},
+							{
+								label: "bills",
+								data: billData.map(e => e.count),
+								backgroundColor: "#5d3f8d",
+								borderColor: "#5d3f8d"
 							},
 						],
 					}}
