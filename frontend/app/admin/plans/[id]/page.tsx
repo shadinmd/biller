effd26955ca657416d926803aecea6cb1c844390
shadinmhd <shadinmhd97@gmail.com>
2024-04-09 08:02:16 +0530

    update: updated types and new theme with edit plan

--- frontend/app/admin/plans/[id]/page.tsx
+++ frontend/app/admin/plans/[id]/page.tsx
@@ -24,7 +24,6 @@ const formSchema = z.object({
 	active: z.boolean(),
 	productLimit: z.number().min(1, { message: "this value cannot be zero" }),
 	billLimit: z.number().min(1, { message: "this value cannot be zero" }),
-	shopLimit: z.number().min(1, { message: "this value cannot be zero" })
 })
 
 type formType = z.infer<typeof formSchema>
@@ -34,7 +33,18 @@ const EditPlan = ({ params }: Props) => {
 
 	const [plan, setPlan] = useState<PlanInterface>()
 
-	const { register, handleSubmit, formState: { errors } } = useForm<formType>({ resolver: zodResolver(formSchema) })
+	const { register, handleSubmit, formState: { errors } } = useForm<formType>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: plan?.name,
+			description: plan?.description,
+			price: plan?.price,
+			discount: plan?.discount,
+			active: plan?.active,
+			productLimit: plan?.productLimit,
+			billLimit: plan?.billLimit,
+		}
+	})
 
 	const inputStyle = (name: formFields) => {
 		return cn("border-2 border-primary rounded-lg px-3 py-1	outline-none", errors[name] ? "border-red-500 placeholder:text-red-500 text-red-500" : "")
@@ -44,7 +54,6 @@ const EditPlan = ({ params }: Props) => {
 		adminApi.get(`/admin/plan/${params.id}`)
 			.then(({ data }) => {
 				if (data.success) {
-					console.log(data.plan)
 					setPlan(data.plan)
 				} else {
 					toast.error(data.message)
@@ -61,7 +70,6 @@ const EditPlan = ({ params }: Props) => {
 
 			if (response.data.success) {
 				toast.success(response.data.message)
-				console.log(response.data)
 			} else {
 				toast.error(response.data.message)
 			}
@@ -71,101 +79,91 @@ const EditPlan = ({ params }: Props) => {
 	}
 
 	return (
-		<div className="flex flex-col items-center justify-center bg-white rounded-lg drop-shadow-lg w-full h-full">
-			<p className="text-3xl">{plan?.name}</p>
+		<div className='flex flex-col gap-5 items-start justify-start p-12 w-full h-full'>
+			<div className='flex flex-col items-center p-2 bg-white drop-shadow-lg rounded-lg'>
+				<p className='text-black font-bold text-xl'>New plan</p>
+			</div>
 			<form
 				onSubmit={handleSubmit(onSubmit)}
-				className='flex items-center gap-5 w-full'
+				className='flex flex-col items-start gap-5 w-full'
 			>
-				<div className='flex flex-col items-center w-full h-full gap-5'>
-					<InputContainer name='Name' error={errors.name?.message}>
-						<input
-							id='name'
-							{...register("name")}
-							defaultValue={plan?.name}
-							placeholder='Name'
-							type="text"
-							className={inputStyle("name")}
-						/>
-					</InputContainer>
-					<InputContainer name='Description' error={errors.description?.message}>
-						<input
-							{...register("description")}
-							defaultValue={plan?.description}
-							placeholder='Descrptrion'
-							type="text"
-							className={inputStyle("description")}
-						/>
-					</InputContainer>
-					<InputContainer name='Price' error={errors.price?.message}>
-						<input
-							{...register("price", { valueAsNumber: true })}
-							placeholder='Price'
-							defaultValue={plan?.price}
-							type="number"
-							className={inputStyle("price")}
-						/>
-					</InputContainer>
-					<InputContainer name='Discount' error={errors.discount?.message}>
-						<input
-							{...register("discount", { valueAsNumber: true })}
-							placeholder='Discount'
-							defaultValue={plan?.discount}
-							type="number"
-							className={inputStyle("discount")}
-						/>
-					</InputContainer>
-				</div>
-				<div className='flex flex-col items-center w-full h-full gap-5'>
-					<InputContainer name='Product limit' error={errors.productLimit?.message}>
-						<input
-							{...register("productLimit", { valueAsNumber: true })}
-							placeholder='Product limit'
-							defaultValue={plan?.productLimit}
-							type="number"
-							className={inputStyle("productLimit")}
-						/>
-					</InputContainer>
-					<InputContainer name='Bill Limit' error={errors.billLimit?.message}>
-						<input
-							{...register("billLimit", { valueAsNumber: true })}
-							placeholder='Staff limit'
-							defaultValue={plan?.billLimit}
-							type="number"
-							className={inputStyle("billLimit")}
-						/>
-					</InputContainer>
-					<InputContainer name='shop Limit' error={errors.shopLimit?.message} >
-						<input
-							{...register("shopLimit", { valueAsNumber: true })}
-							placeholder='Shop Limit'
-							defaultValue={plan?.shopLimit}
-							type="number"
-							className={inputStyle("shopLimit")}
-						/>
-					</InputContainer>
-					<InputContainer name='active' error={errors.active?.message}>
-						<input
-							{...register("active")}
-							defaultChecked={plan?.active}
-							placeholder=''
-							type="checkbox"
-							className={inputStyle("active")}
-						/>
-					</InputContainer>
-					<button className='bg-primary text-white self-end px-6 py-2 font-bold rounded-lg'>
-						Save
-					</button>
-				</div>
+				<InputContainer name='Name' error={errors.name?.message}>
+					<input
+						id='name'
+						{...register("name")}
+						defaultValue={plan?.name}
+						placeholder='Name'
+						type="text"
+						className={inputStyle("name")}
+					/>
+				</InputContainer>
+				<InputContainer name='Description' error={errors.description?.message}>
+					<input
+						{...register("description")}
+						defaultValue={plan?.description}
+						placeholder='Descrptrion'
+						type="text"
+						className={inputStyle("description")}
+					/>
+				</InputContainer>
+				<InputContainer name='Price' error={errors.price?.message}>
+					<input
+						{...register("price", { valueAsNumber: true })}
+						defaultValue={plan?.price}
+						placeholder='Price'
+						type="number"
+						className={inputStyle("price")}
+					/>
+				</InputContainer>
+				<InputContainer name='Discount' error={errors.discount?.message}>
+					<input
+						{...register("discount", { valueAsNumber: true })}
+						placeholder='Discount'
+						defaultValue={plan?.discount}
+						type="number"
+						className={inputStyle("discount")}
+					/>
+				</InputContainer>
+				<InputContainer name='Product limit' error={errors.productLimit?.message}>
+					<input
+						{...register("productLimit", { valueAsNumber: true })}
+						placeholder='Product limit'
+						defaultValue={plan?.productLimit}
+						type="number"
+						className={inputStyle("productLimit")}
+					/>
+				</InputContainer>
+				<InputContainer name='Bill Limit' error={errors.billLimit?.message}>
+					<input
+						{...register("billLimit", { valueAsNumber: true })}
+						defaultValue={plan?.billLimit}
+						placeholder='Bill Limit'
+						type="number"
+						className={inputStyle("billLimit")}
+					/>
+				</InputContainer>
+				<InputContainer name='active' error={errors.active?.message}>
+					<input
+						{...register("active")}
+						checked={plan?.active}
+						placeholder=''
+						type="checkbox"
+						className={inputStyle("active")}
+					/>
+				</InputContainer>
+				<button className='bg-primary text-white px-6 py-2 font-bold rounded-lg'>
+					save
+				</button>
 			</form >
-		</div>
+		</div >
+
 	)
 }
 
 const InputContainer = ({ children, name, error }: { name: string, children: ReactNode, error: string | undefined }) => {
 	return (
-		<div className='flex flex-col'>
-			<div className='flex gap-5'>
+		<div className='flex flex-col bg-white drop-shadow-lg p-2 rounded-lg font-semibold'>
+			<div className='flex items-center gap-5'>
 				<label>{[name[0].toUpperCase(), name.slice(1, name.length)]}: </label>
 				{children}
 			</div>
@@ -173,4 +171,5 @@ const InputContainer = ({ children, name, error }: { name: string, children: Rea
 		</div>
 	)
 }
+
 export default EditPlan
