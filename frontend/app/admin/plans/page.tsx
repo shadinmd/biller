530bc31683225a7f7b4c7c776e9f545bb0192a9b
shadinmhd <shadinmhd97@gmail.com>
2024-04-09 08:00:39 +0530

    update: changed theme for admin plans page

--- frontend/app/admin/plans/page.tsx
+++ frontend/app/admin/plans/page.tsx
@@ -10,7 +10,6 @@ import { adminApi } from "@/lib/adminApi"
 const Plans = () => {
 
 	const [plans, setPlans] = useState<PlanInterface[]>([])
-	const [search, setSearch] = useState("")
 
 	useEffect(() => {
 		adminApi.get("/admin/plan")
@@ -27,64 +26,25 @@ const Plans = () => {
 	}, [])
 
 	return (
-		<div className="flex flex-col gap-3 w-full h-full bg-custom-offwhite">
-			<div className="flex items-center justify-between">
-				<div className="flex gap-3 items-center w-full">
-					<div className="flex items-center gap-2 border-2 bg-white px-3 py-1 rounded-lg w-full">
-						<Icon icon={"mdi:search"} />
-						<input
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-							placeholder="Search"
-							type="text"
-							className="w-full rounded-lg pr-3 outline-none"
-						/>
-					</div>
-				</div>
-				<div className="flex items-center">
-					<Link href={"/admin/newPlan"}>
-						<Icon icon={"mdi:plus"} className="text-4xl text-green-600" />
-					</Link>
-				</div>
+		<div className="flex flex-col gap-1 px-3 py-2 items-center w-full h-full">
+			<div className="flex items-center justify-between w-full">
+				<p className="flex items-center justify-center p-2 font-bold bg-white rounded-lg drop-shadow-lg">Plans</p>
+				<Link href={`/admin/newPlan`} className="bg-white rounded-lg drop-shadow-lg">
+					<Icon icon={"mdi:plus"} className="text-green-500 text-4xl" />
+				</Link>
 			</div>
-			<div className="flex gap-5 items-center w-full h-full">
-				<div className="flex flex-col gap-5  h-full w-full bg-white drop-shadow-lg rounded-lg p-6">
-					<p className="text-black font-bold pb-5">Plans</p>
-					{plans.map((e, i) => (
-						<div key={i} className="flex flex-col p-1 items-start justify-between bg-custom-offwhite rounded-lg">
-							<div className="flex items-center justify-between w-full">
-								<p className="text-lg font-bold">{e.name}</p>
-								<div className="flex items-center gap-1 p-1">
-									<Link href={`/admin/plans/${e?._id}`} className="outline-none">
-										<Icon icon={"mdi:pencil"} className="text-xl" />
-									</Link>
-									<button className="outline-none">
-										<Icon icon={"mdi:trash"} className="text-xl text-red-500" />
-									</button>
-								</div>
-							</div>
-							<div className="flex gap-1">
-								<p>Bill limit: </p>
-								<p>{e.billLimit}</p>
-							</div>
-							<div className="flex gap-1">
-								<p>Shop limit: </p>
-								<p>{e.shopLimit}</p>
-							</div>
-							<div className="flex gap-1">
-								<p>Product limit: </p>
-								<p>{e.productLimit}</p>
-							</div>
-							<div className="flex gap-1">
-								<p>Active: </p>
-								<p>{e.active ? "yes" : "no"}</p>
-							</div>
-						</div>
-					))}
-				</div>
-				<div className="flex flex-col w-[30rem] h-full bg-white rounded-lg drop-shadow-lg">
-
-				</div>
+			<div className="flex flex-col gap-2 w-full">
+				{plans.map((e, i) => (
+					<Link
+						href={`/admin/plans/${e._id}`}
+						key={i}
+						className="flex p-2 items-start justify-between bg-white drop-shadow-lg rounded-lg"
+					>
+						<p className="text-lg font-bold">{e.name}</p>
+						<p className="text-lg font-bold">{e.price - e.discount}₹</p>
+						<p className={`text-lg font-bold ${e.active? "text-green-500" :"text-red-500"}`}>{e.active ? "listed" : "hidden"}</p>
+					</Link>
+				))}
 			</div>
 		</div>
 	)
