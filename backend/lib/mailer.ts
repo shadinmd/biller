1b15c16b20b6b893c22fd9e5b571b5381c757cd7
shadinmhd <shadinmhd97@gmail.com>
2024-04-22 15:22:47 +0530

    update: added content to forgot password mail

--- backend/lib/mailer.ts
+++ backend/lib/mailer.ts
@@ -28,12 +28,19 @@ export const sendOtpMail = (to: string, url: string) => {
 	})
 }
 
-export const sendResetMail = (to: string) => {
+export const sendResetMail = (to: string, url: string) => {
 
 	transporter.sendMail({
 		from: process.env.EMAIL,
 		to,
-		subject: "Biller: email for resetting your password",
-		text: ``
+		subject: "Biller: reset password request",
+		text: ` with biller
+
+				please click the link below to reset the password of your account
+				${url}
+
+				regards,
+				biller team
+`
 	})
 }
