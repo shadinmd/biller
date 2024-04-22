b2f32fef64477ccd12d3c4606def83e1282f1b52
shadinmhd <shadinmhd97@gmail.com>
2024-04-22 11:58:56 +0530

    update: new method for generating unique tokens

--- backend/lib/generator.ts
+++ backend/lib/generator.ts
@@ -1,5 +1,15 @@
-import mongoose from "mongoose"
+import crypto from "crypto"
 
-export const generateRandomString = (): string => {
-	return new mongoose.Types.ObjectId().toHexString()
+function generateToken(input: string): string {
+	const salt = crypto.randomBytes(16).toString('hex')
+
+	const inputWithSalt = `${input}${salt}`
+
+	const sha256 = crypto.createHash('sha256')
+	sha256.update(inputWithSalt)
+	const token = sha256.digest('hex')
+
+	return token
 }
+
+export default generateToken
