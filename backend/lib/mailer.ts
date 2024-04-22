5380c509062c28b22a23cbf698dfe3b4c65fc863
shadinmhd <shadinmhd97@gmail.com>
2024-04-22 12:02:13 +0530

    fix: typo in verification mail

--- backend/lib/mailer.ts
+++ backend/lib/mailer.ts
@@ -20,7 +20,7 @@ export const sendOtpMail = (to: string, url: string) => {
 				please click the link below to verify your account
 				${url}
 
-				If you didn't register in chatbox you can ignore this mail
+				If you didn't register in biller you can ignore this mail
 
 				regards,
 				biller team
