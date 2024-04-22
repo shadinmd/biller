81cbf5c0d334ca016b4e556914a552ea11038395
shadinmhd <shadinmhd97@gmail.com>
2024-04-22 22:26:38 +0530

    update: added full interface for shop and staff

--- types/shop.interface.ts
+++ types/shop.interface.ts
@@ -1,9 +1,23 @@
+import VendorInterface from "./vendor.interface"
+
 interface ShopInterface {
 	_id: string,
 	name: string
 	shop_id: string
 	shop_secret: string
 	vendor: string
+	location: string
+	active: boolean
+	image: string,
+	createdAt: Date
+}
+
+export interface FullShopInterface {
+	_id: string,
+	name: string
+	shop_id: string
+	shop_secret: string
+	vendor: VendorInterface
 	location: string
 	active: boolean
 	image: string,
--- types/staff.interface.ts
+++ types/staff.interface.ts
@@ -1,8 +1,20 @@
+import { FullShopInterface } from "./shop.interface"
+
 interface StaffInterface {
 	_id: string
 	username: string
 	password: string
 	shop: string
+	manager: boolean
+	blocked: boolean
+	createdAt: Date
+}
+
+export interface FullStaffInterface {
+	_id: string
+	username: string
+	password: string
+	shop: FullShopInterface
 	manager: boolean
 	blocked: boolean
 	createdAt: Date
