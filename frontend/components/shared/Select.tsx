76264cf177802748bff98f21a5b635b5cb153728
shadinmhd <shadinmhd97@gmail.com>
2024-04-23 13:41:23 +0530

    fix: added functionality to this component with styling

--- frontend/components/shared/Select.tsx
+++ frontend/components/shared/Select.tsx
@@ -1,27 +1,24 @@
 "use client"
-import { useState } from "react"
 import { ShadSelect, SelectTrigger, SelectContent, SelectGroup, SelectItem } from "../shadcn/ShadSelect"
 
 interface Props {
 	items: string[],
 	onSelect: (item: string) => void
+	selected: string
 }
 
-const Select = ({ items, onSelect }: Props) => {
-	const [selected, setSelected] = useState("Sort by")
+const Select = ({ items, onSelect, selected }: Props) => {
 
 	return (
 		<ShadSelect onValueChange={(e) => onSelect(e)}>
-			<SelectTrigger className="outline-none">
-				<p>
-					{selected}
-				</p>
+			<SelectTrigger className="outline-none bg-white font-bold drop-shadow-lg h-full">
+				{selected}
 			</SelectTrigger>
-			<SelectContent className="outline-none">
+			<SelectContent className="outline-none bg-white">
 				<SelectGroup className="outline-none">
 					{
 						items.map((e, i) => (
-							<SelectItem value={e} key={i} className="outline-none">
+							<SelectItem value={e} key={i} className="outline-none cursor-pointer font-bold">
 								{e}
 							</SelectItem>
 						))
